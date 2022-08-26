8ac3a5cee202a689b488a71b088dbb7555d26456
Jose Carlo Aranas <89120611+carloaranas@users.noreply.github.com>
2022-08-26 14:43:56 +0800

    Update collections.kt

--- collections.kt
+++ collections.kt
@@ -101,3 +101,57 @@ fun main() {
 }
 
 
+//MAPS
+//Creating an Immutable Map
+fun main() {
+  // Write your code below 🏞
+  var codeysMap = mapOf("valley" to "scorpions", "creek" to "snakes", "forest" to "bears") 
+  println(codeysMap)
+  
+}
+
+//Retrieving Keys and Values
+fun main() {
+  var uniqueTransport = mapOf("India" to "Tuktuk", "Egypt" to "Felucca", "Peru" to "Totora Boat", "Hong Kong" to "Junk Boat")
+
+  // Write your code below 🚜
+  println("A unique mode of transportation that exists in Peru is ${uniqueTransport["Peru"]}.")
+  println(uniqueTransport.keys)
+  println(uniqueTransport.values)
+
+}
+
+//Creating a Mutable Map
+fun main() {
+  // Write your code below 📺
+  var tvShows = mutableMapOf("The Big Bang Theory" to 278, "Modern Family" to 250, "Bewitched" to 254, "That '70s Show" to 200)
+  tvShows["The Big Bang Theory"] = 279
+  println(tvShows)
+}
+
+//Adding and Removing Entries
+fun main() {
+  // Write your code below 🖼
+
+  var momaPaintings = mutableMapOf("Les Demoiselles d'avignon" to "Pablo Picasso", "The Starry Night" to "Vincent Van Gogh", "Mona Lisa" to "Leonardo Da Vinci", "The Persistance of Memory" to "Salvador Dali")
+
+  if (!momaPaintings.containsValue("Claude Monet")) {
+    // Write your code for Step 1 here 
+    momaPaintings.put("Water Lillies", "Claude Monet") 
+  }
+
+    // Write your code for Step 2 here
+    momaPaintings.remove("Mona Lisa")
+    println(momaPaintings)
+
+}
+
+//Review
+fun main() {
+  // Write your code below
+  var waifus = mutableMapOf("Raiden Shogun" to "Electro", "Yelan" to "Hydro", "Hu Tao" to "Pyro", "Jean" to "Anemo", "Eula" to "Cryo")
+  println(waifus)
+  
+}
+
+
